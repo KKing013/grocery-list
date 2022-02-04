faec9b859161b1a15d0403ee8e1a7ba2d0ec3643
Kevin King <kjking013@live.com>
2022-02-04 15:13:33 +0200

    Updates

---
+++ precache-manifest.e98f434ae02601582e21d3fa58514b54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9044e6275d5a1251f0469752f0f42d17",
+    "url": "/grocery-list/index.html"
+  },
+  {
+    "revision": "a124961920038bdf9fd5",
+    "url": "/grocery-list/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/grocery-list/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-list/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a124961920038bdf9fd5",
+    "url": "/grocery-list/static/js/main.fe8500dc.chunk.js"
+  },
+  {
+    "revision": "442f1a7391b3cc56a3ca",
+    "url": "/grocery-list/static/js/runtime-main.a51afe8e.js"
+  }
+]);
